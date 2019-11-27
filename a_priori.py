28825158583e58c1283598627bdc235e67e0336e
Philipe Rocha <philipe.bh@gmail.com>
2019-11-27 16:39:17 -0300

    apriori.sigma - base

--- a_priori.py
+++ a_priori.py
@@ -8,61 +8,8 @@ Philipe Rocha
 """
 
 data = []
-
-
-# suporte de cada item/ pensei em usar uma estrutura de (chave:valor) para armazenar isso;
-def sigma(citem):
-    sig = 0
-    sc = set(citem)
-    for d in data:
-        if sc.issubset(d):
-            sig += 1
-
-    return sig
-
-
-# função para gerar os candidatos a partir de F -> funcionando
-
-def genCandidatos(f):
-    listC = []
-    fc = f.copy()
-    while fc:
-        t = fc.pop()
-        for c in fc:
-            u = c | t
-            if len(u) == len(c) + 1:
-                listC.append(u)
-    return listC
-
-
-# função que gera o set com os conjuntos que estão nas transações -> funcionando
-
-def subSet(C, t):
-    lc = []
-    for c in C:
-        if t.issuperset(c):
-            lc.append(c)
-    return lc
-
-
-def apriori(supMin, transacoes, itens):
-    k: int = 1
-    f_list = [{i} for i in itens if sigma(i) >= supMin]  # itemset de 1 elemento -> funciona, basta a função sigma
-    f = fk = set(f_list)
-    while len(fk) != 0:
-        k += 1
-        ck = genCandidatos(f)
-        for t in transacoes:
-            ct = subSet(ck, t)
-            for c in ct:
-                c
-                # incrementa o suporte -> fiquei com dúvida nessa parte..
-        fk_list = [{c} for c in ck if sigma(c) >= supMin]
-        fk = set(fk_list)
-        f.union(fk)
-
-    return f
-
+itens_data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-17]
+supMin = 0.50
 
 def norData():
     ref = open("house_votes_84.data", "r")
@@ -101,17 +48,81 @@ def norData():
     return 1
 
 
+# suporte de cada item/ pensei em usar uma estrutura de (chave:valor) para armazenar isso;
+def sigma(citem):
+    sig = 0
+    sc = set(citem)
+    for d in data:
+        ld = set(d)
+        if sc.issubset(ld):
+            sig += 1
+
+    return sig
+
+
+# função para gerar os candidatos a partir de F -> funcionando
+
+def genCandidatos(f):
+    listC = []
+    fc = f.copy()
+    while fc:
+        t = fc.pop()
+        for c in fc:
+            u = c | t
+            if len(u) == len(c) + 1:
+                listC.append(u)
+    return listC
+
+
+# função que gera o set com os conjuntos que estão nas transações -> funcionando
+
+def subSet(C, t):
+    lc = []
+    t = set(t)
+    for c in C:
+        if t.issuperset(c):
+            lc.append(c)
+    return lc
+
+def uniList(l1, l2):
+    lr = l2.copy()
+    for x in l1:
+        if x not in lr:
+            if len(x) > 1:
+                lr.append(x)
+    return lr
+
+def apriori(supMin, transacoes, itens):
+    k: int = 1
+    Reult = []
+    # f_list = [{i} for i in itens ]  # itemset de 1 elemento -> funciona, basta a função sigma
+    f_list = [{i} for i in itens if sigma([i])/len(data) >= supMin]  # itemset de 1 elemento -> funciona, basta a função sigma
+    f = fk = f_list
+    while len(fk) != 0:
+        k += 1
+        ck = genCandidatos(f)
+
+        for t in transacoes:
+            ct = subSet(ck, t)
+            for c in ct:
+                c
+                # incrementa o suporte -> fiquei com dúvida nessa parte..
+        fk_list = [c for c in ck if sigma(list(c))/len(data) >= supMin]
+        if len(fk_list) == 0: break
+        f = uniList(f, fk_list)
+        fk_list.clear()
+
+    return f
+
+
 def main():
     # t = {1, -2, 3}
-    #
     # t1 = [{1}, {-2}, {-3}]
-    # x = [{'pao', 'leite'}, {'leite', 'cafe'}, {'ovo', 'cafe'}]
-    #
-    # print(subSet(genCandidatos(x), t))
-    # print(subSet(t1, t))
-    # print(genCandidatos(x))
+    # x1 = [{'pao', 'leite'}, {'leite', 'cafe'}, {'ovo', 'cafe'}]
+    # z = set(x)
     norData()
+    x = apriori(supMin, data, itens_data)
 
-    print(sigma([]))
+    print(x)
 
 main()
